a0144cb46733860d59bd6551c048ce6380f33b78
Nicholas Strydom <nstrydom@hallfieldcontrols.com>
2024-10-03 11:28:37 -0500

    Add project files.

---
+++ FizzBuzz/FizzBuzz.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    public class FizzBuzz
+    {
+        public bool IsDivisibleBy3(int num)
+        {
+            return num % 3 == 0;
+        }
+
+        public bool IsDivisibleBy5(int num)
+        {
+            // TODO implement a method that returns `true` if the num variable
+            // is divisible by 5
+            return num % 5 == 0;
+        }
+
+        public void IterFizzBuzz(int num)
+        {
+            for (int i = 0; i < num; i++)
+            {
+                if (IsDivisibleBy3(i) && IsDivisibleBy5(i))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+
+                else if (IsDivisibleBy3(i)) { Console.WriteLine("Fizz"); }
+
+                else if (IsDivisibleBy5(i)) { Console.WriteLine("Buzz"); }
+
+                else { Console.WriteLine(i); }
+            }
+        }
+    }
+}
---
+++ FizzBuzz/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+namespace FizzBuzz {
+    internal class Program {
+        static void Main(string[] args)
+        {
+            var fizzbuzz = new FizzBuzz();
+            fizzbuzz.IterFizzBuzz(20);
+        }
+    }
+}
